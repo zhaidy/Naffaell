69e59c033098119f100d4e43f952f3bfd89d195b
zhaidy <dongyuzhai@gmail.com>
2015-10-30 14:03:36 +0800

    Added Webservice

--- App_Code/GetPlayer.cs
+++ App_Code/GetPlayer.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Services;
 
@@ -17,11 +19,155 @@ public class GetPlayer : System.Web.Services.WebService {
 
         //Uncomment the following line if using designed components 
         //InitializeComponent(); s
+        
     }
+
 
     [WebMethod]
     public string HelloWorld() {
         return "Hello World";
     }
-    
+
+    [WebMethod]
+    public player_profile PlayerProfile(string server, string playerId)
+    {
+        string _playerId = playerId;
+        string _server = server;
+
+        player_profile _player_profile = new player_profile(); //Player Profile : player_id, server, fighting
+        string url = "http://lolbox.duowan.com/playerDetail.php?serverName=" + server + "&playerName=" + playerId;
+        string fighting = "";
+        string level = "";
+        string playerIcon = "";
+        string first_win = "";
+
+        var getHtmlWeb = new HtmlWeb();
+        var htmlDocument = getHtmlWeb.Load(url);
+        HtmlNode bodyNode = htmlDocument.DocumentNode.SelectSingleNode("//body");
+
+        //战斗力 : update_datetime, fighting
+        IEnumerable<HtmlNode> fightingNodes = htmlDocument.DocumentNode.Descendants().Where(x => x.Name == "div" && x.Attributes.Contains("class")
+            && x.Attributes["class"].Value.Split().Contains("fighting"));
+        foreach (HtmlNode child in fightingNodes)
+        {
+            HtmlNode node = child.SelectSingleNode("//span");
+            fighting = Regex.Match(child.InnerText, @"\d+").Value;
+        }
+
+        //get icon, level
+        IEnumerable<HtmlNode> profileNodes = htmlDocument.DocumentNode.Descendants().Where(x => x.Name == "div" && x.Attributes.Contains("class")
+            && x.Attributes["class"].Value.Split().Contains("avatar"));
+        foreach (HtmlNode child in profileNodes)
+        {
+            if (!child.InnerHtml.Contains("yy"))
+            {
+                level = child.SelectSingleNode("//em").InnerText;
+                playerIcon = Regex.Match(child.InnerHtml, "<img.+?src=[\"'](.+?)[\"'].*?>", RegexOptions.IgnoreCase).Groups[1].Value;
+            }
+        }
+
+        //get first win
+        IEnumerable<HtmlNode> firstWinNodes = htmlDocument.DocumentNode.Descendants().Where(x => x.Name == "div" && x.Attributes.Contains("class")
+            && x.Attributes["class"].Value.Split().Contains("act"));
+        foreach (HtmlNode child in firstWinNodes)
+        {
+            first_win = child.InnerText.Replace("&nbsp;", "");
+        }
+
+        //add profile
+        _player_profile.player_id =  _playerId;
+        _player_profile.server =  server;
+        _player_profile.fighting =  fighting;
+        _player_profile.icon =  playerIcon;
+        _player_profile.first_win =  first_win;
+        _player_profile.level = level;
+
+        return _player_profile;
+    }
+
+    [WebMethod]
+    public com_heros ComHeros(string server, string playerId)
+    {
+        string _playerId = playerId;
+        string _server = server;
+
+        com_heros _com_heros = new com_heros();
+        string url = "http://lolbox.duowan.com/playerDetail.php?serverName=" + server + "&playerName=" + playerId;
+
+        var getHtmlWeb = new HtmlWeb();
+        var htmlDocument = getHtmlWeb.Load(url);
+        HtmlNode bodyNode = htmlDocument.DocumentNode.SelectSingleNode("//body");
+
+        IEnumerable<HtmlNode> com_heroNodes = htmlDocument.DocumentNode.Descendants().Where(x => x.Name == "div" && x.Attributes.Contains("class")
+            && x.Attributes["class"].Value.Split().Contains("com-hero"));
+        _com_heros.com_hero_list = new List<com_hero>();
+        foreach (HtmlNode child in com_heroNodes)
+        {
+            if (child.NodeType != HtmlNodeType.Text)
+            {
+                HtmlNodeCollection nodes = child.SelectNodes("//li");
+                foreach (HtmlNode node in nodes)
+                {
+                    string _count = Regex.Match(Regex.Match(node.InnerHtml, @"\d次").Value, @"\d").Value;
+                    if (node.Attributes.Contains("champion-name-ch"))
+                    {
+                        _com_heros.com_hero_list.Add(new com_hero ()
+                        {
+                            champion_name_ch = node.Attributes["champion-name-ch"].Value,
+                            champion_name = node.Attributes["champion-name"].Value,
+                            icon = Regex.Match(node.InnerHtml, "<img.+?src=[\"'](.+?)[\"'].*?>", RegexOptions.IgnoreCase).Groups[1].Value,
+                            count = _count
+                        });
+                    }
+                }
+            }
+        }
+        return _com_heros;
+    }
+
+    public class player_profile
+    {
+        public string player_id { get; set; }
+        public string server { get; set; }
+        public string icon { get; set; }
+        public string level { get; set; }
+        public string fighting { get; set; }
+        public string first_win { get; set; }
+    }
+    public class normal_statistics
+    {
+        public string type { get; set; }
+        public string total_matches { get; set; }
+        public string win_rate { get; set; }
+        public string matches_winned { get; set; }
+        public string matches_lost { get; set; }
+        public string update_datetime { get; set; }
+    }
+    public class rank_statistics
+    {
+        public string type { get; set; }
+        public string rank { get; set; }
+        public string point { get; set; }
+        public string total_matches { get; set; }
+        public string win_rate { get; set; }
+        public string matches_winned { get; set; }
+        public string matches_lost { get; set; }
+        public string update_datetime { get; set; }
+    }
+    public class fighting
+    {
+        public string update_datetime { get; set; }
+        public string _fighting { get; set; }
+    }
+    public class com_heros
+    {
+        public List<com_hero> com_hero_list { get; set; }
+    }
+    public class com_hero
+    {
+        public string champion_name_ch { get; set; }
+        public string champion_name { get; set; }
+        public string icon { get; set; }
+        public string count { get; set; }
+    }
 }
